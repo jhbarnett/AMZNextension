c3f2ecfb02d73ca2aa64c6c0c0b2ed67765455b2
Jason Barnett <jason@thebarnetts.ws>
2017-01-18 14:03:33 -0600

    searches amazon for product

---
+++ src/content.js
@@ -0,0 +1,11 @@
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      let product = document.querySelector("title").innerHTML.split(' ').join('+')
+      console.log(product)
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": `https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=${product}`})
+    }
+  }
+)
+
