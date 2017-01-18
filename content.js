032bacdbd92707fc7452c1e09f4b58438df6d01b
Jason Barnett <jason@thebarnetts.ws>
2017-01-18 12:51:09 -0600

    basic setup - opens new amazon tab

---
+++ content.js
@@ -0,0 +1,12 @@
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": "https://www.amazon.com"});
+
+    }
+
+
+  }
+);
