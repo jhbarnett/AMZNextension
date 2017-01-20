cb00e27a7a5b5a30c5fae77440f29990e4cedbe3
Jason Barnett <jason@thebarnetts.ws>
2017-01-19 18:37:40 -0600

    sends product to bg

--- src/background.js
+++ src/background.js
@@ -1,18 +1,18 @@
 // background.js
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
+chrome.webNavigation.onCompleted.addListener(function(e){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    if (activeTab) chrome.tabs.sendMessage(activeTab.id, {"message": "page_loaded"});
   });
-});
+})
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "open_new_tab" ) {
       chrome.tabs.create({"url": request.url})
+    }
+    else if( request.message === "fetch_amazon_products" ) {
+      alert(request.product)
     }
   }
 );
